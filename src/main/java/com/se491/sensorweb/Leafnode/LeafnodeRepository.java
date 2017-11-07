6191fb60e21f501f9a7d590e02eeb5461696b10e
Gregory Steenhagen <gregorysteenhagen@gregorys-mbp.student.iastate.edu>
2017-11-07 15:25:27 -0600

    Got homenode api working so you can add readings

--- src/main/java/com/se491/sensorweb/Leafnode/LeafnodeRepository.java
+++ src/main/java/com/se491/sensorweb/Leafnode/LeafnodeRepository.java
@@ -1,10 +1,14 @@
 package com.se491.sensorweb.Leafnode;
 
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface LeafnodeRepository extends CrudRepository<Leafnode, Long> {
 
     Leafnode findByLeafnodeIdAndHomenodeId(Long leafnodeId, Long homenodeId);
+
+    ArrayList<Leafnode> findByHomenodeId(Long homenodeId);
 }
